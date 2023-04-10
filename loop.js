523c761b6d76703766fa53199573b7c6dcc09e44
Prominents <leonhardt722@gmail.com>
2023-04-10 23:41:37 +0700

    New looping

--- loop.js
+++ loop.js
@@ -22,6 +22,17 @@ do {
   z++;
 } while (z <= 10);
 
+//For statement 'of' for getValue, 'in' for getIndex
+const myArray = ["Amelia", "Harper", "Evelyn", "Abigail", "Emily"];
+for (let listArray of myArray) {
+  console.log(listArray);
+}
+
+//For statement 'each' & 'map' are same for get Value and Index
+myArray.forEach((value, index) => {
+  console.log(value, index+1);
+});
+
 //loop & sum using array data
 const a = [3, 5, 12];
 let sum = 0;
